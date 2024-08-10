44c079ce4ba0f65d17854bb61f496a9aadc379d6
anujd4348 <anujd4348@gmail.com>
2024-08-10 04:59:06 +0000

    it is what it iss

--- zomato-clone-frontend/src/App.jsx
+++ zomato-clone-frontend/src/App.jsx
@@ -5,12 +5,17 @@ import Cart from "./pages/Cart/Cart";
 import Home from "./pages/Home/Home";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
+    {showLogin?<LoginPopup/>:<></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
