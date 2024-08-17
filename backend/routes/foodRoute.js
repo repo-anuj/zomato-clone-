bec3b92294e921eff8653b7b8339ad1dcad5d5d0
anujd4348 <anujd4348@gmail.com>
2024-08-17 16:57:17 +0000

    storage has been created

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -4,7 +4,16 @@ import multer from "multer"
 
 const foodRouter = express.Router();
 
+// Image Storage Engine
 
+const storage = multer.diskStorage({
+    destination:"uploads",
+    filename:(req,file,cb)=>{
+        return cb(null,`${Date.now()}${file.originalname}`)
+    }
+})
+
+foodRouter.post("/app",addFood)
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
 
 
 
@@ -16,6 +17,7 @@ app.use(cors())
 connectDB();
 
 // api endpoints
+app.use("/api/food",foodRouter)
 
 
 app.get("/",(req,res)=>{
