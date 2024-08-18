8d852614aa7fa00aa187acbf34f1caab21939dba
anujd4348 <anujd4348@gmail.com>
2024-08-18 16:02:00 +0000

    ...........

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <hr/>
+      <div className="app-content">
+        <Sidebar />
+      </div>
+    </div>
   )
 }
 
